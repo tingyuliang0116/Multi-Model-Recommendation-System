7279bbf2a11aa1604b71a2a1a6bc3331893332bf
michelle <mich910116@gmail.com>
2025-06-18 15:21:43 +0800

    Dockerfile for popularity recommender service

--- src/popularity_recommender/main.py
+++ src/popularity_recommender/main.py
@@ -2,18 +2,20 @@ import os
 import pandas as pd
 from pydantic import BaseModel
 import logging
-from config.environment_config import Config
 import mlflow
 import mlflow.pyfunc
+from fastapi import FastAPI
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-config = Config()
 app = FastAPI(title="Popularity-Based Recommendation Engine")
 
 
 LOADED_MODEL = None
-
+MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI", "http://localhost:5000")
+MINIO_HOST = os.getenv("MINIO_HOST", "localhost:9000") # Default to localhost for local Docker
+MINIO_ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY", "minioadmin")
+MINIO_SECRET_KEY = os.getenv("MINIO_SECRET_KEY", "minioadmin")
 
 class RecommendationRequest(BaseModel):
     user_id: str | None = None  
@@ -33,11 +35,10 @@ class ProductRecommendation(BaseModel):
 async def startup_event():
     global LOADED_MODEL
 
-    mlflow.set_tracking_uri("http://127.0.0.1:5000")
-    MINIO_HOST = config.MINIO_BIND["endpoint"]
+    mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
     os.environ['MLFLOW_S3_ENDPOINT_URL'] = f"http://{MINIO_HOST}"
-    os.environ['AWS_ACCESS_KEY_ID'] = config.MINIO_BIND["access_key"]
-    os.environ['AWS_SECRET_ACCESS_KEY'] = config.MINIO_BIND["secret_key"]
+    os.environ['AWS_ACCESS_KEY_ID'] = MINIO_ACCESS_KEY
+    os.environ['AWS_SECRET_ACCESS_KEY'] = MINIO_SECRET_KEY
     LOADED_MODEL = mlflow.pyfunc.load_model(
         model_uri=f"models:/PopularityRecommenderModel@champion")
 
